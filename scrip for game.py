a3e567eaffcf1d4cd3405be84ce0aadbfd542c6f
danik292 <116908854+danik292@users.noreply.github.com>
2023-03-27 14:22:33 +0000

    yes

--- scrip for game.py
+++ scrip for game.py
@@ -5,25 +5,18 @@ import random
 
 name = name = input("What is your name? ")
  #say nice to meet you name
-    print("Nice to meet you " + name)
+print("Nice to meet you " + name)
 #ask for age
-    age = input("How old are you? ")
-#if age is more then 18 say you can play the game
-    if age is > 18:        print("You can play the game")
-    else:
-        print("You can't play the game")
-    #print plase write a number between 1 and 10 when the nuber is same as my random number you obitain a point
-    number = random.randint(1,10)
-    print("I am thinking of a number between 1 and 10")
-#ask for guess  
-    guess = input("Take a guess: ")
-#if guess is equal to my random number you obitain a point  
-    if int(guess) == number:
-        print("You got it right")
-    else:
-        print("You got it wrong")
-#if guess is same than my random number you obitain a point
-    if int(guess) == number:
-        print("You got it right")
-    else:
-        print("You got it wrong")
+age = input("How old are you? ")
+
+        # MAKE A VARIABLE TO STORE THE POINTS
+        points = 0
+        while points < 3:
+            guess = input("Take a guess: ")
+            if int(guess) == number:
+                print("You got it right")
+                points += 1
+            else:
+                print("You got it wrong")
+                points += 1¨
+                print("You have " + str(points) + " points")
